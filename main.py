e42f118200ceaa1ddc8237e895999df497cd526f
lenlino <65199014+lenlino@users.noreply.github.com>
2023-09-09 13:55:49 +0900

    databaseのインポートより先にenvをロードする。

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ import discord
 import discord.ext.commands.cog
 from dotenv import load_dotenv
 
+load_dotenv()
+
 import utils.DataBase
 from commands import CardCommand
 from generate.generate import get_json_from_url, generate_panel
 from utils.DataBase import get_connection
 
-load_dotenv()
+
 bot = discord.AutoShardedBot()
 token = os.environ.get('HONKAI_TOKEN')
 
