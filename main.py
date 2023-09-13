37ad0dcdce8b28935cc19cee4318a4cedaa6a852
lenlino <65199014+lenlino@users.noreply.github.com>
2023-09-13 10:41:13 +0900

    アクティビティの表示

--- main.py
+++ main.py
@@ -4,15 +4,12 @@ import os
 import asyncpg as asyncpg
 import discord
 import discord.ext.commands.cog
+from discord.ext import tasks
 from dotenv import load_dotenv
 
 load_dotenv()
 
 import utils.DataBase
-from commands import CardCommand
-from generate.generate import get_json_from_url, generate_panel
-from utils.DataBase import get_connection
-
 
 bot = discord.AutoShardedBot()
 token = os.environ.get('HONKAI_TOKEN')
@@ -20,6 +17,7 @@ token = os.environ.get('HONKAI_TOKEN')
 
 async def init_bot():
     await utils.DataBase.init()
+    status_update_task.start()
 
 
 @bot.event
@@ -27,6 +25,12 @@ async def on_ready():
     await init_bot()
     print("起動しました")
     # await generate_panel()
+
+
+@tasks.loop(minutes=10)
+async def status_update_task():
+    text = f"正常稼働中　Servers: {str(len(bot.guilds))}"
+    await bot.change_presence(activity=discord.CustomActivity(text))
 
 
 bot.load_extension('commands.CardCommand')
