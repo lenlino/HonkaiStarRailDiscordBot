f0e30c74600403d8d82f07a6df03924dd6162fd3
lenlino <65199014+lenlino@users.noreply.github.com>
2023-11-15 17:56:38 +0900

    キャラ位置がおかしいバグの修正

--- backend/backend.py
+++ backend/backend.py
@@ -1,18 +1,20 @@
 import io
 
-from flask import Flask
-from flask import send_file
+from fastapi import APIRouter
+from starlette.responses import StreamingResponse
 
 import generate.generate
 
-app = Flask(__name__)
-app.debug = True
+router = APIRouter()
 
-@app.route("/")
-async def hello_world():
+
+@router.get("/gen_card/{uid}")
+async def gen_card(uid: str, select_number: int, is_uid_hide: bool, calculation_value: str, lang: str):
     image_binary = io.BytesIO()
-    panel_img = await generate.generate.generate_panel(template=2)
-    panel_img.save(image_binary, 'PNG')
+    panel_img = await generate.generate.generate_panel(uid=uid, chara_id=int(select_number), template=2,
+                                                       is_hideUID=is_uid_hide
+                                                       , calculating_standard=calculation_value, lang=lang)
+    panel_img['img'].save(image_binary, 'PNG')
     image_binary.seek(0)
-    return send_file(image_binary, mimetype='image/png')
-
+    print(image_binary)
+    return StreamingResponse(content=image_binary, media_type="image/png")
--- main.py
+++ main.py
@@ -1,24 +1,28 @@
-import io
+import asyncio
 import os
 
-import asyncpg as asyncpg
 import discord
 import discord.ext.commands.cog
 from discord.ext import tasks
 from dotenv import load_dotenv
+from fastapi import FastAPI
 
-import backend.backend
 import i18n
+from backend.backend import router
 
 i18n.load_path.append(f"{os.path.dirname(os.path.abspath(__file__))}/i18n")
 i18n.set('fallback', 'en')
 
 load_dotenv()
 
+# dotenv読み込んでからimport
 import utils.DataBase
 
 bot = discord.AutoShardedBot()
 token = os.environ.get('HONKAI_TOKEN')
+
+#app = FastAPI()
+#app.include_router(router)
 
 
 async def init_bot():
@@ -39,5 +43,9 @@ async def status_update_task():
     await bot.change_presence(activity=discord.CustomActivity(text))
 
 
+async def run():
+    await bot.start(token)
+
+
 bot.load_extension('commands.CardCommand')
-bot.run(token)
+#asyncio.create_task(run())
