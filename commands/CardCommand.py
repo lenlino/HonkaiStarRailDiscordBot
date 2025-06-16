e58fda82cbbcfa5916dc6454338a154ffb8866c3
lenlino <65199014+lenlino@users.noreply.github.com>
2025-06-16 17:05:09 +0900

    🐛 fix: デフォルト値未設定時の処理を修正
    - chara_typesが空の場合に正しいフォールバック動作を追加
    - リストが存在する場合、最初の要素にデフォルト値を設定するよう変更

--- commands/CardCommand.py
+++ commands/CardCommand.py
@@ -90,10 +90,14 @@ class CardCommand(commands.Cog):
 
             # デフォルト値が設定されなかった場合のフォールバック
             if not is_defalut_set:
-                calculation_value = "no_score"
-                chara_types.append(
-                    discord.SelectOption(label=i18n.t("message.no_score", locale=lang), value="no_score", default=True)
-                )
+                if len(chara_types) == 0:
+                    calculation_value = "no_score"
+                    chara_types.append(
+                        discord.SelectOption(label=i18n.t("message.no_score", locale=lang), value="no_score",
+                                             default=True)
+                    )
+                else:
+                    chara_types[0].default = True
             else:
                 chara_types.append(
                     discord.SelectOption(label=i18n.t("message.no_score", locale=lang), value="no_score", default=False)
