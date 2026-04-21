3d35a616bd8b85db8ec558bf4f590e206d1cc509
lenlino <65199014+lenlino@users.noreply.github.com>
2026-04-22 07:15:03 +0900

    result_json の return を追加

--- generate/utils.py
+++ generate/utils.py
@@ -62,6 +62,7 @@ async def get_json_from_url(uid: str, lang: str):
         async with session.get(f"{be_address}/sr_info_parsed/{uid}?lang={lang}") as response:
             if response.status == 200:
                 result_json = await response.json()
+    return result_json
     if len(result_json.keys()) == 0 or "detail" in result_json:
         filepath = pathlib.Path(f"{os.path.dirname(os.path.abspath(__file__))}/StarRailRes/index_min/{lang}")
         index = CustomIndex(filepath)
